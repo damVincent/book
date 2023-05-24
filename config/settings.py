1ddd0ce7f409bc5c6e6696e98350a26886fc8a19
damVincent <112225511+damVincent@users.noreply.github.com>
2023-05-24 23:40:00 +0200

    .

--- config/settings.py
+++ config/settings.py
@@ -15,9 +15,9 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://*.herokuapp.com', 'https://*.127.0.0.1', 'https://*.localhost',]
+ALLOWED_HOSTS = ['https://prose-palace.herokuapp.com', 'https://*.127.0.0.1', 'https://*.localhost',]
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com', 'https://*.127.0.0.1', 'https://*.localhost',]
-PREPEND_WWW = True 
+
 # Application definition
 
 INSTALLED_APPS = [
